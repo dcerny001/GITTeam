e7afae2aadc2d464a01cfddb8a546295d3e2bc39
dcerny <dcerny@student.tgm.ac.at>
2018-02-13 11:41:38 +0100

    Erster Commit.

---
+++ src/cerny/HelloTeam.java
@@ -0,0 +1,12 @@
+package cerny;
+
+public class HelloTeam {
+	
+	public static void hallovonCerny(){
+		System.out.print("Schönes Sommersemester" + " wünscht Cerny!");
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
